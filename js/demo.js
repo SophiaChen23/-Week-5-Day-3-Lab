ea4adee6a5740599472bf582cbbbf025f7a6333e
sophia <zchen9523@gmail.com>
2023-06-08 18:24:13 -0500

    fade the whole pics

---
+++ js/demo.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    /* Open lightbox on button click */
+    $('.lightbox-toggle img').click(function(){
+        $('.backdrop').animate({'opacity':'.50'}, 300, 'linear').css('display', 'block');
+        $('.box').fadeIn();
+
+        //Check if lightbox has an image
+        if ($('.box').contents('img')) {
+            $('.box').contents().remove('img'); //If true, clear image
+        }
+
+        //Get text content in attribute
+        var $altvalue = $(this).attr('alt'); //or var altvalue = $(this).attr('alt');
+
+        var img = $(this).clone();
+        $('.box').append(img); //Insert duplicated element in another element
+
+    });
+
+    /* Click to close lightbox */
+    $('.close, .backdrop').click(function(){
+        $('.backdrop').animate({'opacity':'0'}, 300, 'linear', function(){
+            $('.backdrop').css('display', 'none');
+        });
+        $('.box').fadeOut();
+    });
+
+});
